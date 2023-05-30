2585fd6c1dd34ac3eb915fced3ae49752a93d501
Aitor Blesa <aitor.ble@gmail.com>
2023-05-30 11:19:03 +0200

    last changes to best practices

--- 04-ToDoApp/src/App.jsx
+++ 04-ToDoApp/src/App.jsx
@@ -4,14 +4,14 @@ import Filter from "./components/Filter"
 import PersonForm from './components/PersonForm'
 
 const App = () => {
-
-  const [persons, setPersons] = useState([
+  const initialPersons = [
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
     { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
     { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  ]
 
+  const [persons, setPersons] = useState(initialPersons)
   const [newName, setNewName] = useState('')
   const [newPhone, setnewPhone] = useState('')
   const [filteredPersons, setFilteredPersons] = useState(persons)
@@ -30,7 +30,7 @@ const App = () => {
   }
 
   const addNote = () => {
-    const existsName = persons.find(person => person.name === newName);
+    const existsName = persons.some(person => person.name === newName);
 
     if (existsName) {
       alert(`${newName} is already added to the PhoneBook`)
@@ -51,6 +51,8 @@ const App = () => {
       <Filter handleSearch={handleSearch} />
       <h2>Add a new</h2>
       <PersonForm
+        newName={newName}
+        newPhone={newPhone}
         handleChange={handleChange}
         handleChangePhone={handleChangePhone}
         onSubmit={addNote}>
